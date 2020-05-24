887513d0c4a7c2758535bbb5a08c480a0e13abbb
Redhouane Sobaihi <s-redouane@outlook.com>
2020-05-24 23:36:45 +0700

    Clean film

---
+++ src/Films.WebSite/Migrations/20200524131449_AddFilmEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmsLibrary.Migrations
+{
+    public partial class AddFilmEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "catalog");
+
+            migrationBuilder.CreateTable(
+                name: "films",
+                schema: "catalog",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    year = table.Column<short>(type: "smallint", nullable: false),
+                    director = table.Column<string>(type: "nvarchar(MAX)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_films", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "films",
+                schema: "catalog");
+        }
+    }
+}
---
+++ src/Films.WebSite/Migrations/20200524134730_AddIdentity.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmsLibrary.Migrations
+{
+    public partial class AddIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "creator_id",
+                schema: "catalog",
+                table: "films",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "catalog",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
+                    Email = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_films_creator_id",
+                schema: "catalog",
+                table: "films",
+                column: "creator_id");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                schema: "catalog",
+                table: "users",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                schema: "catalog",
+                table: "users",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_films_users_creator_id",
+                schema: "catalog",
+                table: "films",
+                column: "creator_id",
+                principalSchema: "catalog",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_films_users_creator_id",
+                schema: "catalog",
+                table: "films");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "catalog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_films_creator_id",
+                schema: "catalog",
+                table: "films");
+
+            migrationBuilder.DropColumn(
+                name: "creator_id",
+                schema: "catalog",
+                table: "films");
+        }
+    }
+}
