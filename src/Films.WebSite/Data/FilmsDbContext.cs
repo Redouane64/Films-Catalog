957d166c13424b79168f0cf5012c9a1890e21c8d
Redhouane Sobaihi <s-redouane@outlook.com>
2020-05-24 19:53:23 +0700

    Add project files.

---
+++ src/Films.WebSite/Data/FilmsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Films.WebSite.Data
+{
+    public class FilmsDbContext : DbContext
+    {
+        public FilmsDbContext(DbContextOptions<FilmsDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
